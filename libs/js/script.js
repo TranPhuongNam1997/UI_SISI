2ca24753a63c876fe2cccd4ea0fef8d74b99197b
TranPhuongNam1997 <tranphuongnam17041997@gmail.com>
2021-07-19 00:28:16 +0700

    u

--- libs/js/script.js
+++ libs/js/script.js
@@ -2,25 +2,24 @@ $( document ).ready(function() {
 
     //nut scroll top
 
-    // $("#back-to-top").click(function () {
-    //     $("html, body").animate({scrollTop : 0},"slow");
-    //     return false;
-    // });
-    // $(window).scroll(function () {
-    //     if ($(window).scrollTop() >=300) {
-    //         $('#back-to-top').show();
-    //     }
-    //
-    //     else {
-    //         $('#back-to-top').hide();
-    //     }
-    //
-    // });
+    $(".back-to-top").click(function () {
+        $("html, body").animate({scrollTop : 0},"slow");
+        return false;
+    });
+    $(window).scroll(function () {
+        if ($(window).scrollTop() >=300) {
+            $('.back-to-top').addClass('active');
+        }
+
+        else {
+            $('.back-to-top').removeClass('active');
+        }
+
+    });
 
 });
 
 $(function() {
-    // $('.swiper-slide-active .heading-banner').addClass('fadeInDown');
 
     $('.ripple').on('click', function(event) {
         event.preventDefault();
